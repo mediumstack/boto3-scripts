84b170bff3fa1c07be0383654363db29fafd4a2f
maxstack <maxstack@null>
2018-05-11 15:47:24 +1200

    edited for clarity

--- create_instance.py
+++ create_instance.py
@@ -1,14 +1,18 @@
 #!/user/bin/env python
 
-import boto3
-import yaml
 import sys
+import yaml
+import boto3
 
+# break if no args give
+# otherwise set the instance name var
 if len(sys.argv) == 1:
     print "Please provide an instance name, Eg:\n<script_name> my_new_instance"
     sys.exit(1)
+else:
+    instance_name = sys.argv[1]
 
-instance_name = sys.argv[1]
+# set the instance_id var to blank for later
 instance_id = None
 
 conf = yaml.load(open('conf/aws.config'))
@@ -16,6 +20,7 @@ config_aws_access_key_id = conf['aws']['access_key_id']
 config_aws_secret_access_key = conf['aws']['secret_access_key']
 config_region_name = conf['aws']['region_name']
 
+# set the instance variables from the conf file
 security_group = conf['instance']['sg']
 ssh_key = conf['instance']['ssh_key']
 flavour = conf['instance']['flavour']
@@ -46,6 +51,8 @@ def create_default_tags(instance_id, instance_name):
     """
     Set the default tags for deployment
     """
+
+    # you could duplicate this line if you have extra default tags
     ec2.create_tags(Resources=[instance_id], Tags=[{'Key':'Name', 'Value':instance_name}])
 
 def main():
