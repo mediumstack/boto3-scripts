35e5e72163f2e9e5454a3ef8930515d9ea01d8b4
maxstack <maxstack@null>
2018-05-17 15:21:09 +1200

    Add a new column for launch date
    Needs to be updated to add column titles

--- list_instances.sh
+++ list_instances.sh
@@ -12,5 +12,5 @@ STATE=$1
 if [ $STATE = "running" ]; then
 	aws ec2 describe-instances --query 'Reservations[].Instances[].[InstanceId, Tags[?Key==`Name`].Value | [0], InstanceType, LaunchTime, State.Name, PublicIpAddress, KeyName]' --filters Name=instance-state-name,Values=$STATE --output table --region ap-southeast-2
 else
-	aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value | [0], InstanceType, State.Name]' --filters Name=instance-state-name,Values=$STATE --output table --region ap-southeast-2
+	aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value | [0], InstanceType, LaunchTime, State.Name]' --filters Name=instance-state-name,Values=$STATE --output table --region ap-southeast-2
 fi
